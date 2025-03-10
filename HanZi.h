e44c9fa5afa938ad52794da13476eb2eb1ab435a
zhh <146307966+zhh300@users.noreply.github.com>
2025-03-10 23:22:33 +0800

    Add files via upload

---
+++ HanZi.h
@@ -0,0 +1,24 @@
+#ifndef __HANZI_H
+#define __HANZI_H 
+
+
+#define	u8 unsigned char
+#define	u16 unsigned int
+#define	u32 unsigned long
+////////////////////////
+//begin
+extern const unsigned char asc2_1608[];
+extern const unsigned char gImage_pic1[];
+extern const unsigned char gImage_pic2[];
+extern const unsigned char gImage_pic3[];
+extern const unsigned char image1[];
+//extern u8 Hzk[];
+extern unsigned char hanzi[];
+
+int char_to_num(char c);
+
+
+//end
+////////////////////
+
+#endif 
